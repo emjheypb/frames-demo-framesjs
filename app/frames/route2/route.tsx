1b6c88655cc79884d97c17a58da2af9724d1278e
MJ Baysic <mjpb123@gmail.com>
2024-07-05 00:27:13 -0400

    TEST: read context input text in route 2

--- app/frames/route2/route.tsx
+++ app/frames/route2/route.tsx
@@ -4,9 +4,16 @@ import { Button } from "frames.js/next";
 
 export const POST = frames(async (cntxt) => {
   let state = cntxt.state;
+  let message = cntxt.message?.inputText
+    ? `, Typed: ${cntxt.message?.inputText}`
+    : "";
 
   return {
-    image: <div tw="flex">Route 2 count is {state.count}</div>,
+    image: (
+      <div tw="flex">
+        Route 2 count is {state.count} {message}
+      </div>
+    ),
     buttons: [
       <Button action="post" target="/route1">
         Go to route 1
