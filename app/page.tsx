4ce8d36eb3d1e6c3173eef0fd988525137f04d66
EmJhey <emjheypb@gmail.com>
2024-07-08 14:38:22 -0400

    TEST: fc metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,37 @@
 import { fetchMetadata } from "frames.js/next";
+import { Metadata } from "next";
 
 export async function generateMetadata() {
   return {
-    title: "My Page",
-    // provide a full URL to your /frames endpoint
-    other: {
-      // ...
-      ...(await fetchMetadata(
-        // provide full URL to your /frames endpoint
-        new URL(
-          "/frames",
+    title: "New Format",
+    description:
+      "New format because the prev code doesn't work anymore for some reason",
+    openGraph: {
+      title: "New Format",
+      description:
+        "New format because the prev code doesn't work anymore for some reason",
+      images: [
+        `${
           process.env.VERCEL_URL
             ? `https://${process.env.VERCEL_URL}`
             : "http://localhost:3000"
-        )
-      )),
+        }/vercel.svg`,
+      ],
+    },
+    // provide a full URL to your /frames endpoint
+    other: {
+      "fc:frame": "vNext",
+      "fc:frame:image": `${
+        process.env.VERCEL_URL
+          ? `https://${process.env.VERCEL_URL}`
+          : "http://localhost:3000"
+      }/vercel.svg`,
+      "fc:frame:post_url": `${
+        process.env.VERCEL_URL
+          ? `https://${process.env.VERCEL_URL}`
+          : "http://localhost:3000"
+      }/frames`,
+      "fc:frame:button:1": `Start`,
     },
   };
 }
@@ -22,7 +39,7 @@ export async function generateMetadata() {
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      My existing page
+      Hello, world!
     </main>
   );
 }
