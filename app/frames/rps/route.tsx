fb725c862e667060d9d4ee1107176701c7e59c9f
EmJhey <emjheypb@gmail.com>
2024-07-09 16:20:32 -0400

    FIX: scoring

--- app/frames/rps/route.tsx
+++ app/frames/rps/route.tsx
@@ -12,26 +12,18 @@ export const POST = frames(async (ctx) => {
   const choice = ctx.searchParams.choice;
   const cpuChoice = Math.floor(Math.random() * 3);
 
-  const win = () => {
-    state = { ...state, pScore: state.pScore + 1 };
-  };
-
-  const lose = () => {
-    state = { ...state, cScore: state.cScore + 1 };
-  };
-
   switch (choice) {
     case "r":
-      if (cpuChoice === 1) lose;
-      else if (cpuChoice === 2) win;
+      if (cpuChoice === 1) state = { ...state, cScore: state.cScore + 1 };
+      else if (cpuChoice === 2) state = { ...state, pScore: state.pScore + 1 };
       break;
     case "p":
-      if (cpuChoice === 2) lose;
-      else if (cpuChoice === 0) win;
+      if (cpuChoice === 2) state = { ...state, cScore: state.cScore + 1 };
+      else if (cpuChoice === 0) state = { ...state, pScore: state.pScore + 1 };
       break;
     case "s":
-      if (cpuChoice === 0) lose;
-      else if (cpuChoice === 1) win;
+      if (cpuChoice === 0) state = { ...state, cScore: state.cScore + 1 };
+      else if (cpuChoice === 1) state = { ...state, pScore: state.pScore + 1 };
       break;
   }
 
@@ -44,7 +36,9 @@ export const POST = frames(async (ctx) => {
             {cpuChoice === 0 ? "🪨" : cpuChoice === 1 ? "📄" : "✂️"}
           </p>
         )}
+        <br />
         <p>You: {state.pScore}</p>
+        <br />
         <p>CPU: {state.cScore}</p>
       </div>
     ),
